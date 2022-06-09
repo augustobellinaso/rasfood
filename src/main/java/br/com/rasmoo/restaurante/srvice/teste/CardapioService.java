c54466df96827e16b293475436b6153598ef7e0b
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2022-06-08 23:08:49 -0300

    refatorando a classe prato para Cardapio

--- src/main/java/br/com/rasmoo/restaurante/srvice/teste/CardapioService.java
+++ src/main/java/br/com/rasmoo/restaurante/srvice/teste/CardapioService.java
@@ -1,50 +1,50 @@
 package br.com.rasmoo.restaurante.srvice.teste;
 
-import br.com.rasmoo.restaurante.dao.PratoDao;
-import br.com.rasmoo.restaurante.entity.Prato;
+import br.com.rasmoo.restaurante.dao.CardapioDao;
+import br.com.rasmoo.restaurante.entity.Cardapio;
 import br.com.rasmoo.restaurante.util.JPAUtil;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
 
-public class PratoService {
+public class CardapioService {
 
 	public static void main(String[] args) {
 
-		Prato risoto = new Prato();
+		Cardapio risoto = new Cardapio();
 		risoto.setNome("Risoto de frutos do mar");
 		risoto.setDescricao("Risoto acompanhado de lula, polvo e mariscos");
 		risoto.setDisponivel(true);
 		risoto.setValor(BigDecimal.valueOf(88.50));
 
-		Prato salmao = new Prato();
+		Cardapio salmao = new Cardapio();
 		salmao.setNome("Salmão ao molho de maracujá");
 		salmao.setDescricao("Salmão grelhado ao molho de maracujá");
 		salmao.setDisponivel(true);
 		salmao.setValor(BigDecimal.valueOf(60.00));
 
 		EntityManager entityManager = JPAUtil.getEntityManagerRasfood();
-		PratoDao pratoDao = new PratoDao(entityManager);
+		CardapioDao cardapioDao = new CardapioDao(entityManager);
 
 		entityManager.getTransaction().begin();
-		pratoDao.cadastrar(risoto);
-		pratoDao.cadastrar(salmao);
+		cardapioDao.cadastrar(risoto);
+		cardapioDao.cadastrar(salmao);
 
-		logaPratoConsultado(1, pratoDao);
+		logaPratoConsultado(1, cardapioDao);
 
-		pratoDao.excluir(salmao);
-		logaPratoConsultado(2, pratoDao);
+		cardapioDao.excluir(salmao);
+		logaPratoConsultado(2, cardapioDao);
 
 		risoto.setDisponivel(false);
-		pratoDao.atualizar(risoto);
+		cardapioDao.atualizar(risoto);
 
-		logaPratoConsultado(1, pratoDao);
+		logaPratoConsultado(1, cardapioDao);
 		entityManager.getTransaction().commit();
 		entityManager.close();
 
 	}
 
-	private static void logaPratoConsultado(Integer id, PratoDao pratoDao) {
-		System.out.println("O prato consultado foi ==> " + pratoDao.consultar(id));
+	private static void logaPratoConsultado(Integer id, CardapioDao cardapioDao) {
+		System.out.println("O prato consultado foi ==> " + cardapioDao.consultar(id));
 	}
 }

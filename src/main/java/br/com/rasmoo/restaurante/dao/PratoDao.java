cce40639d36c82b0418099232a3ce0143e4e9308
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2022-06-08 22:59:55 -0300

    implementando os metodos do CRUD

--- src/main/java/br/com/rasmoo/restaurante/dao/PratoDao.java
+++ src/main/java/br/com/rasmoo/restaurante/dao/PratoDao.java
@@ -6,14 +6,28 @@ import javax.persistence.EntityManager;
 
 public class PratoDao {
 
-	private EntityManager entityManager;
+	private final EntityManager entityManager;
 
 	public PratoDao(EntityManager entityManager) {
 		this.entityManager = entityManager;
 	}
 
-	public void cadastrar(Prato prato) {
+	public void cadastrar(final Prato prato) {
 		this.entityManager.persist(prato);
 		System.out.println("Entidade cadastrada ==> " + prato);
+	}
+
+	public Prato consultar(final Integer id) {
+		return this.entityManager.find(Prato.class, id);
+	}
+
+	public void atualizar(final Prato prato) {
+		this.entityManager.merge(prato);
+		System.out.println("Entidade atualizada ==> " + prato);
+	}
+
+	public void excluir(final Prato prato) {
+		this.entityManager.remove(prato);
+		System.out.println("Entidade removida ==> " + prato);
 	}
 }

a3a05e0063489ed82ee236ef3527d958e4d7d6d9
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2022-06-08 23:25:13 -0300

    testando o relacionamento

--- src/main/java/br/com/rasmoo/restaurante/entity/Cardapio.java
+++ src/main/java/br/com/rasmoo/restaurante/entity/Cardapio.java
@@ -29,6 +29,14 @@ public class Cardapio {
 	public Cardapio() {
 	}
 
+	public Cardapio(String nome, String descricao, Boolean disponivel, BigDecimal valor, Categoria categoria) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.disponivel = disponivel;
+		this.valor = valor;
+		this.categoria = categoria;
+	}
+
 	public Integer getId() {
 		return id;
 	}
--- src/main/java/br/com/rasmoo/restaurante/srvice/teste/CardapioService.java
+++ src/main/java/br/com/rasmoo/restaurante/srvice/teste/CardapioService.java
@@ -1,7 +1,9 @@
 package br.com.rasmoo.restaurante.srvice.teste;
 
 import br.com.rasmoo.restaurante.dao.CardapioDao;
+import br.com.rasmoo.restaurante.dao.CategoriaDao;
 import br.com.rasmoo.restaurante.entity.Cardapio;
+import br.com.rasmoo.restaurante.entity.Categoria;
 import br.com.rasmoo.restaurante.util.JPAUtil;
 
 import javax.persistence.EntityManager;
@@ -11,37 +13,48 @@ public class CardapioService {
 
 	public static void main(String[] args) {
 
+		EntityManager entityManager = JPAUtil.getEntityManagerRasfood();
+		Categoria categoria = cadastrarCategoria(entityManager);
+		cadastrarProdutoCardapio(entityManager, categoria);
+
+	}
+
+	private static Categoria cadastrarCategoria(EntityManager entityManager) {
+		CategoriaDao categoriaDao = new CategoriaDao(entityManager);
+		Categoria pratoPrincipal = new Categoria("Prato Principal");
+		entityManager.getTransaction().begin();
+		categoriaDao.cadastrar(pratoPrincipal);
+		entityManager.getTransaction().commit();
+		entityManager.clear();
+
+		return pratoPrincipal;
+	}
+
+	private static void cadastrarProdutoCardapio(EntityManager entityManager, Categoria categoria) {
 		Cardapio risoto = new Cardapio();
 		risoto.setNome("Risoto de frutos do mar");
 		risoto.setDescricao("Risoto acompanhado de lula, polvo e mariscos");
 		risoto.setDisponivel(true);
 		risoto.setValor(BigDecimal.valueOf(88.50));
+		risoto.setCategoria(categoria);
 
 		Cardapio salmao = new Cardapio();
 		salmao.setNome("Salmão ao molho de maracujá");
 		salmao.setDescricao("Salmão grelhado ao molho de maracujá");
 		salmao.setDisponivel(true);
 		salmao.setValor(BigDecimal.valueOf(60.00));
+		salmao.setCategoria(categoria);
 
-		EntityManager entityManager = JPAUtil.getEntityManagerRasfood();
 		CardapioDao cardapioDao = new CardapioDao(entityManager);
-
 		entityManager.getTransaction().begin();
 		cardapioDao.cadastrar(risoto);
+		entityManager.flush();
 		cardapioDao.cadastrar(salmao);
-
+		entityManager.flush();
 		logaPratoConsultado(1, cardapioDao);
-
-		cardapioDao.excluir(salmao);
 		logaPratoConsultado(2, cardapioDao);
 
-		risoto.setDisponivel(false);
-		cardapioDao.atualizar(risoto);
-
-		logaPratoConsultado(1, cardapioDao);
-		entityManager.getTransaction().commit();
 		entityManager.close();
-
 	}
 
 	private static void logaPratoConsultado(Integer id, CardapioDao cardapioDao) {

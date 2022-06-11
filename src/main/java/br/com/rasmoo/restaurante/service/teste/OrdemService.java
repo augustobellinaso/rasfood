c966dbbc90cd3e79092cf7f0043b8b03b38f0d37
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2022-06-11 14:07:19 -0300

    criando a entidade endereco

--- src/main/java/br/com/rasmoo/restaurante/service/teste/OrdemService.java
+++ src/main/java/br/com/rasmoo/restaurante/service/teste/OrdemService.java
@@ -3,10 +3,7 @@ package br.com.rasmoo.restaurante.service.teste;
 import br.com.rasmoo.restaurante.dao.CardapioDao;
 import br.com.rasmoo.restaurante.dao.ClienteDao;
 import br.com.rasmoo.restaurante.dao.OrdemDao;
-import br.com.rasmoo.restaurante.entity.Cardapio;
-import br.com.rasmoo.restaurante.entity.Cliente;
-import br.com.rasmoo.restaurante.entity.Ordem;
-import br.com.rasmoo.restaurante.entity.OrdensCardapio;
+import br.com.rasmoo.restaurante.entity.*;
 import br.com.rasmoo.restaurante.util.JPAUtil;
 import br.com.rasmoo.restaurante.util.PopulaDadosUtil;
 
@@ -24,7 +21,9 @@ public class OrdemService {
 		ClienteDao clienteDao = new ClienteDao(entityManager);
 		OrdemDao ordemDao = new OrdemDao(entityManager);
 
-		Cliente augusto = new Cliente("12345678900", "Augusto", "12345000");
+		Endereco endereco = new Endereco("12345000", "Rua 1", "Casa", "Viajada", "RS");
+		Cliente augusto = new Cliente("12345678900", "Augusto");
+		augusto.addEndereco(endereco);
 		Ordem ordem = new Ordem(augusto);
 		ordem.addOrdensCardapio(new OrdensCardapio(cardapioDao.consultarPorId(1), 2));
 
